12ffb52e2cf86bc3d20d5a5139552ce677611b0d
AurelienMonteillet <aurelien.monteillet@nomadic-labs.com>
2024-07-31 11:51:37 +0200

    remove fonction findport

--- bin/main.js
+++ bin/main.js
@@ -5,9 +5,10 @@ const { execSync, exec } = require('child_process');
 const os = require('os');
 const path = require('path');
 const configureServiceUnit = require('../lib/serviceManager');
-const { checkPortInUse, findAvailablePort, detectExistingNodes } = require('../lib/detect');
+const { checkPortInUse, detectExistingNodes } = require('../lib/detect');
 const fs = require('fs');
 const sudo = require('sudo-prompt');
+const downloadFile = require('../lib/downloadFile');
 
 const BASE_DIR = os.homedir();
 
@@ -30,11 +31,9 @@ async function main() {
         ]);
 
         if (!continueInstallation) {
-            console.log('Installation annulée par l\'utilisateur.');
+            console.log('Installation annulée.');
             process.exit(0);
         }
-    } else {
-        console.log('Aucun nœud Tezos existant trouvé.');
     }
 
     const { network } = await inquirer.prompt([
@@ -64,106 +63,48 @@ async function main() {
     let netPort;
 
     while (true) {
-        rpcPort = await inquirer.prompt([
+        const answers = await inquirer.prompt([
             {
                 type: 'input',
                 name: 'rpcPort',
                 message: 'Entrez le port RPC à utiliser (ou appuyez sur Entrée pour utiliser le port par défaut 8732):',
-                default: '8732',
-                validate: async (input) => {
-                    const port = parseInt(input, 10);
-                    if (isNaN(port)) {
-                        return 'Veuillez entrer un numéro de port valide.';
-                    }
-                    const inUse = await checkPortInUse(port);
-                    if (inUse) {
-                        return `Le port ${port} est déjà utilisé. Veuillez en choisir un autre.`;
-                    }
-                    return true;
-                }
-            }
-        ]);
-
-        rpcPort = rpcPort.rpcPort;
-
-        netPort = await inquirer.prompt([
+                default: '8732'
+            },
             {
                 type: 'input',
                 name: 'netPort',
                 message: 'Entrez le port réseau à utiliser (ou appuyez sur Entrée pour utiliser le port par défaut 9732):',
-                default: '9732',
-                validate: async (input) => {
-                    const port = parseInt(input, 10);
-                    if (isNaN(port)) {
-                        return 'Veuillez entrer un numéro de port valide.';
-                    }
-                    const inUse = await checkPortInUse(port);
-                    if (inUse) {
-                        return `Le port ${port} est déjà utilisé. Veuillez en choisir un autre.`;
-                    }
-                    return true;
-                }
+                default: '9732'
             }
         ]);
 
-        netPort = netPort.netPort;
+        const rpcPortInUse = await checkPortInUse(answers.rpcPort);
+        const netPortInUse = await checkPortInUse(answers.netPort);
 
-        if (!(await checkPortInUse(rpcPort)) && !(await checkPortInUse(netPort))) {
-            break;
+        if (rpcPortInUse) {
+            console.log(`Le port RPC ${answers.rpcPort} est déjà utilisé. Veuillez choisir un autre port.`);
+        } else if (netPortInUse) {
+            console.log(`Le port réseau ${answers.netPort} est déjà utilisé. Veuillez choisir un autre port.`);
         } else {
-            console.log(`Les ports choisis (${rpcPort}, ${netPort}) sont déjà utilisés. Veuillez en choisir d'autres.`);
-        }
-    }
-
-    let nodeName;
-    let customPath;
-    let dataDir;
-
-    while (true) {
-        const answers = await inquirer.prompt([
-            {
-                type: 'input',
-                name: 'nodeName',
-                message: 'Voulez-vous personnaliser le nom du nœud? (laisser vide pour le nom par défaut):',
-                default: `.${network}-node`
-            },
-            {
-                type: 'input',
-                name: 'customPath',
-                message: 'Voulez-vous personnaliser l\'emplacement du nœud? (laisser vide pour l\'emplacement par défaut):',
-                default: BASE_DIR
-            }
-        ]);
-
-        nodeName = answers.nodeName;
-        customPath = answers.customPath;
-        dataDir = path.join(customPath, nodeName);
-
-        if (fs.existsSync(dataDir)) {
-            const { overwrite } = await inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'overwrite',
-                    message: `Le dossier ${dataDir} existe déjà. Voulez-vous le supprimer et réinstaller?`,
-                    default: false
-                }
-            ]);
-
-            if (overwrite) {
-                fs.rmSync(dataDir, { recursive: true });
-                console.log(`Le dossier ${dataDir} a été supprimé.`);
-                fs.mkdirSync(dataDir, { recursive: true });
-                break;
-            } else {
-                console.log(`Veuillez choisir un autre nom ou emplacement.`);
-            }
-        } else {
-            fs.mkdirSync(dataDir, { recursive: true });
+            rpcPort = answers.rpcPort;
+            netPort = answers.netPort;
             break;
         }
     }
 
-    const { snapshotMode } = await inquirer.prompt([
+    const { nodeName, customPath, snapshotMode } = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'nodeName',
+            message: 'Voulez-vous personnaliser le nom du nœud? (laisser vide pour le nom par défaut):',
+            default: `.${network}-node`
+        },
+        {
+            type: 'input',
+            name: 'customPath',
+            message: 'Voulez-vous personnaliser l\'emplacement du nœud? (laisser vide pour l\'emplacement par défaut):',
+            default: BASE_DIR
+        },
         {
             type: 'list',
             name: 'snapshotMode',
@@ -175,83 +116,60 @@ async function main() {
         }
     ]);
 
+    const dataDir = path.join(customPath, nodeName === `.${network}-node` ? nodeName : `.${nodeName}`);
     const fastMode = snapshotMode === 'fast';
 
-    async function initializeNode() {
-        console.log(`Initialisation du noeud...`);
-        try {
-            execSync(`octez-node config init --data-dir ${dataDir} --network=${network} --history-mode=${mode}`);
-        } catch (error) {
-            console.error('Erreur lors de l\'initialisation du noeud:', error.message);
-            process.exit(1);
-        }
-
-        console.log(`Lancement du noeud pour création de l'identité...`);
-        const nodeProcess = exec(`octez-node run --data-dir ${dataDir}`);
-
-        try {
-            await waitForIdentityFile(dataDir);
-            console.log('Identité créée, arrêt du noeud...');
-            nodeProcess.kill('SIGINT');
-        } catch (error) {
-            console.error('Erreur lors de la création de l\'identité:', error.message);
-            nodeProcess.kill('SIGINT');
-            cleanNodeData(dataDir); // Clean the data inside the directory
-            console.log('Tentative de réinitialisation du noeud...');
-            await initializeNode(); // Retry node initialization
-        }
+    if (fs.existsSync(dataDir)) {
+        console.log(`Le dossier ${dataDir} existe déjà. Veuillez choisir un autre nom.`);
+        process.exit(1);
     }
 
-    await initializeNode();
+    fs.mkdirSync(dataDir, { recursive: true });
+
+    console.log(`Initialisation du noeud...`);
+    execSync(`octez-node config init --data-dir ${dataDir} --network=${network} --history-mode=${mode}`);
+    console.log(`Lancement du noeud pour création de l'identité...`);
+    const nodeProcess = exec(`octez-node run --data-dir ${dataDir}`);
+
+    try {
+        await waitForIdentityFile(dataDir);
+        console.log('Identité créée, arrêt du noeud...');
+        nodeProcess.kill('SIGINT');
+    } catch (error) {
+        console.error(error.message);
+        cleanNodeData(dataDir);
+        console.log('Le dossier de données a été nettoyé. Réinitialisation...');
+        execSync(`octez-node config init --data-dir ${dataDir} --network=${network} --history-mode=${mode}`);
+        const nodeProcessRetry = exec(`octez-node run --data-dir ${dataDir}`);
+        try {
+            await waitForIdentityFile(dataDir);
+            console.log('Identité créée lors de la réinitialisation, arrêt du noeud...');
+            nodeProcessRetry.kill('SIGINT');
+        } catch (retryError) {
+            console.error(retryError.message);
+            process.exit(1);
+        }
+    }
 
     console.log('Arrêt du noeud...');
     execSync(`sudo systemctl stop octez-node`);
 
-    const snapshotUrl = `https://snapshots.eu.tzinit.org/${network}/${mode}`;
-    const snapshotPath = path.join('/tmp', 'snapshot');
-
-    console.log(`Téléchargement du snapshot depuis ${snapshotUrl}...`);
-    await downloadFile(snapshotUrl, snapshotPath);
-
     try {
         cleanNodeData(dataDir);
-        await importSnapshot(network, mode, dataDir, fastMode, snapshotPath);
+        await importSnapshot(network, mode, dataDir, fastMode);
     } catch (error) {
-        console.error('Erreur lors de l\'importation du snapshot:', error.message);
+        console.error('Erreur lors de l\'importation du snapshot:', error);
         console.log('Tentative de nettoyage et nouvelle importation du snapshot...');
         cleanNodeData(dataDir);
         try {
-            await importSnapshot(network, mode, dataDir, fastMode, snapshotPath);
-        } catch (importError) {
-            console.error('Nouvelle erreur lors de l\'importation du snapshot:', importError.message);
+            await importSnapshot(network, mode, dataDir, fastMode);
+        } catch (retryError) {
+            console.error('Nouvelle erreur lors de l\'importation du snapshot:', retryError);
             process.exit(1);
         }
     }
 
-    console.log('Configuration du service...');
-    try {
-        configureServiceUnit(dataDir, rpcPort, netPort);
-    } catch (error) {
-        console.error('Erreur lors de la configuration du service:', error.message);
-        process.exit(1);
-    }
-
-    try {
-        await new Promise((resolve, reject) => {
-            sudo.exec(`systemctl restart octez-node`, { name: 'octez-node-setup' }, (error, stdout, stderr) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    console.log('Le service octez-node a été redémarré.');
-                    resolve();
-                }
-            });
-        });
-    } catch (error) {
-        console.error('Erreur lors du redémarrage du service octez-node:', error.message);
-        process.exit(1);
-    }
-
+    configureServiceUnit(dataDir, rpcPort, netPort);
     console.log('Installation terminée.');
 }
 
